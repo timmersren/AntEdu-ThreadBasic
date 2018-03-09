79a130a9f11372513555969af2cac51e6595af66
任伟 <459001452@qq.com>
2018-03-09 10:26:43 +0800

    更新代码

---
+++ src/cn/rain/thread/basic/example/batch/SendMessageThread.java
@@ -0,0 +1,27 @@
+package cn.rain.thread.basic.example.batch;
+
+import java.util.List;
+
+/**  
+* description:   
+* @author 任伟  
+* @date Mar 8, 2018
+*/
+public class SendMessageThread implements Runnable{
+
+	private List<User> lstUser;
+	
+	public SendMessageThread(List<User> lstUser) {
+		super();
+		this.lstUser = lstUser;
+	}
+
+	@Override
+	public void run() {
+		for (User user : lstUser) {
+			System.out.println("正在给" + user.getUsername() + "发送信息..." + Thread.currentThread().getName());
+		}
+		System.out.println();
+	}
+	
+}
---
+++ src/cn/rain/thread/security/demo/SynchronizedBlockThread.java
@@ -0,0 +1,34 @@
+package cn.rain.thread.security.demo;
+
+/**  
+* description: 使用synchronized同步代码块解决线程安全问题
+* @author 任伟  
+* @date Mar 8, 2018
+*/
+public class SynchronizedBlockThread implements Runnable {
+
+	private Object obj = new Object();
+	private int ticketCount = 100;
+	
+	@Override
+	public void run() {
+		while (ticketCount > 0) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			sale();
+		}
+	}
+	
+	public void sale(){
+		
+		synchronized (obj) {
+		if (ticketCount > 0) {
+			System.out.println(Thread.currentThread().getName() + "正在出售第" + (100-ticketCount+1) + "张票");
+			ticketCount --;
+			}
+		}
+	}
+}
